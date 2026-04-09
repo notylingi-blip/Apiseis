3ea95528355cb102d87ba491c224b5529819e732
notylingi-blip <notylingi@gmail.com>
2026-04-09 13:10:34 +0800

    Update 21HubDuel.lua

--- 21HubDuel.lua
+++ 21HubDuel.lua
@@ -938,7 +938,7 @@ end
 local C = {
     bg         = Color3.fromRGB(0,70,180),
     bgRow      = Color3.fromRGB(24,24,24),
-    white      = Color3.fromRGB(255,255,255),
+    white      = Color3.fromRGB(224,242,254),
     dim        = Color3.fromRGB(150,150,150),
     muted      = Color3.fromRGB(85,85,85),
     off        = Color3.fromRGB(48,48,48),
